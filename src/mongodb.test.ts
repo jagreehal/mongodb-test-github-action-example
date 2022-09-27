33495efdc1860bf4a46a9a8b2dbde74b5b9938cd
Jag Reehal <jag.reehal@gmail.com>
2022-09-27 19:21:15 +0100

    fixed casing in import

--- src/mongodb.test.ts
+++ src/mongodb.test.ts
@@ -1,5 +1,5 @@
 import { connect, disconnect } from 'mongoose';
-import { User } from './models/user';
+import { User } from './models/User';
 
 describe('User Model Test', () => {
   beforeAll(async () => {
