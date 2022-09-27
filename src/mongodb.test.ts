a1289d28cef8b291f01cbc3be30df76c71946d41
Jag Reehal <jag.reehal@gmail.com>
2022-09-27 19:19:15 +0100

    initial commit

---
+++ src/mongodb.test.ts
@@ -0,0 +1,24 @@
+import { connect, disconnect } from 'mongoose';
+import { User } from './models/user';
+
+describe('User Model Test', () => {
+  beforeAll(async () => {
+    await connect('mongodb://localhost:27017/test');
+  });
+  afterAll(async () => {
+    await User.deleteMany();
+    await disconnect();
+  });
+  it('create & save user successfully', async () => {
+    const user = new User({
+      name: 'Bill',
+      email: 'bill@initech.com',
+      avatar: 'https://i.imgur.com/dM7Thhn.png',
+    });
+
+    await user.save();
+
+    const savedUser = await User.findOne({ name: 'Bill' });
+    expect(savedUser.name).toBe('Bill');
+  });
+});
